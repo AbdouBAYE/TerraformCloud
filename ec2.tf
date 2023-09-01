4b8d641daf4c61a7c581ceb2145f44894096d0db
Abdoulayedevops <117050560+AbdouBAYE@users.noreply.github.com>
2023-09-01 12:23:59 +0200

    Create ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region     = "us-east-1"
+}
+
+resource "aws_instance" "myec2" {
+  ami           = "ami-012cc038cc685a0d7"
+  instance_type = "t2.micro"
+  key_name      = "devops-abdou"
+
+  tags = {
+    Name = "ec2-abdou"
+  }
+ 
+}
